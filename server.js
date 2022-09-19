693c3d583e898c371becf37e7913973bd7a85c5a
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-09-19 13:44:45 +0530

    First graphql endpoint and the graphql server were created.

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { graphql, buildSchema } = require("graphql");
+const { graphqlHTTP } = require("express-graphql");
+
+const schema = buildSchema(`
+    type Query {
+        description: String,
+        price: Float
+    }
+`);
+
+const root = {
+  description: "Red Shoe",
+  price: 42.12,
+};
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema, // this gives us the shape of our data
+    rootValue: root,
+  })
+);
+
+const PORT = 8000;
+app.listen(PORT, () => {
+  console.log("Running GraphQL server...");
+});
