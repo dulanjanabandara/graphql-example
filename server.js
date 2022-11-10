f5325c40dd283984843da95d141e84500a0c6d72
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-11 05:18:00 +0530

    The example graphql server.

--- server.js
+++ server.js
@@ -1,66 +1,17 @@
 const express = require("express");
-const { graphql, buildSchema } = require("graphql");
+const { buildSchema } = require("graphql");
 const { graphqlHTTP } = require("express-graphql");
 
 const schema = buildSchema(`
     type Query {
-        products: [Product]
-        orders: [Order]
-    }
-
-    type Product {
-        id: ID!
-        description: String!
-        reviews: [Review]
-        price: Float!
-    }
-
-    type Review {
-        rating: Int!
-        comment: String
-    }
-
-    type Order {
-        date: String!
-        subtotal: Float!
-        items: [OrderItem]
-    }
-
-    type OrderItem {
-        product: Product!
-        quantity: Int!
+        description: String
+        price: Float
     }
 `);
 
 const root = {
-  products: [
-    {
-      id: "redshoe",
-      description: "Red Shoe",
-      price: 42.12,
-    },
-    {
-      id: "bluejean",
-      description: "Blue Jeans",
-      price: 55.55,
-    },
-  ],
-  orders: [
-    {
-      date: "2005-05-05",
-      subtotal: 90.22,
-      items: [
-        {
-          product: {
-            id: "redshoe",
-            description: "Old Red Shoe",
-            price: 45.11
-          },
-          quantity: 2,
-        },
-      ],
-    },
-  ],
+  description: "Red Shoe",
+  price: 42.12,
 };
 
 const app = express();
@@ -68,13 +19,12 @@ const app = express();
 app.use(
   "/graphql",
   graphqlHTTP({
-    schema: schema, // this gives us the shape of our data
+    schema: schema,
     rootValue: root,
-    graphiql: true, // enabling graphiql
+    graphiql: true,
   })
 );
 
-const PORT = 8000;
-app.listen(PORT, () => {
+app.listen(4000, () => {
   console.log("Running GraphQL server...");
 });
