e9e9781b1de7cf63ca8f8c793a631401648cf03b
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-11 08:18:01 +0530

    Used graphql-tools and restructured the folder structure.

---
+++ orders/orders.model.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    date: "2005-05-05",
+    subtotal: 90.22,
+    items: [
+      {
+        product: {
+          id: "readshoe",
+          description: "Old Red Shoe",
+          price: 45.11,
+        },
+        quantity: 2,
+      },
+    ],
+  },
+];
