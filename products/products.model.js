bb03b893c7a9eb1ed0a7681516dad91f7771e8e6
Dulanjana Bandara <dulanjanauop@gmail.com>
2022-11-12 03:19:55 +0530

    Mutations were added to products.

--- products/products.model.js
+++ products/products.model.js
@@ -3,11 +3,13 @@ const products = [
     id: "redshoe",
     description: "Red Shoe",
     price: 42.12,
+    reviews: [],
   },
   {
     id: "bluejean",
     description: "Blue Jeans",
     price: 55.55,
+    reviews: [],
   },
 ];
 
@@ -22,11 +24,50 @@ function getProductByPrice(min, max) {
 }
 
 function getProductById(id) {
-  return products.find((product) => id === product.id);
+  return products.find((product) => {
+    return id === product.id;
+  });
+}
+
+function addNewProduct(id, description, price) {
+  const newProduct = {
+    id,
+    description,
+    price,
+    reviews: [],
+  };
+  products.push(newProduct);
+  return newProduct;
+}
+
+// function addNewProductReview(id, rating, comment) {
+//   return products.find((product) => {
+//     if (id === product.id) {
+//       const review = {
+//         rating,
+//         comment,
+//       };
+//       return product.reviews.push(review);
+//     }
+//   });
+// }
+
+function addNewProductReview(id, rating, comment) {
+  const matchedProduct = getProductById(id);
+  if (matchedProduct) {
+    const newProductReview = {
+      rating,
+      comment,
+    };
+    matchedProduct.reviews.push(newProductReview);
+    return newProductReview;
+  }
 }
 
 module.exports = {
   getAllProducts,
   getProductByPrice,
   getProductById,
+  addNewProduct,
+  addNewProductReview,
 };
